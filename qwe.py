090ee38ea6cd4ed8e0eaf2af40f922af154e6789
NLUfuk <ufukf1998@gmail.com>
2025-04-24 22:30:42 +0300

    while tru qwe added

---
+++ qwe.py
@@ -0,0 +1,8 @@
+while True:
+        print("\n===== To-Do List =====")
+        print("1. Add Task")
+        print("2. Show Tasks")
+        print("3. Mark Task as Done")
+        print("4. Exit")
+
+        choice = input("Enter your choice: ")
