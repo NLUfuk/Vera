af811cc924641876f51a4dc6349ab204ef9d1872
NLUfuk <ufukf1998@gmail.com>
2025-05-13 23:19:36 +0300

    rerevize

--- olasılık.py
+++ olasılık.py
@@ -8,14 +8,14 @@ t2p = int(input("enter t2 puan"))
 
 
 # Takım 2'nin 5 maçını da simüle eden versiyon
-def simulate_full_5_matches(start_points_team1, start_points_team2):
+def simulate_full_matches(start_points_team1, start_points_team2):
     outcomes = [0, 1, 3]
     total_cases = 0
     champion_cases = 0
 
     for t1_results in product(outcomes, repeat=rp1):
         t1_total = start_points_team1 + sum(t1_results)
-        for t2_results in product(outcomes, repeat=rp2):  # 5 maç
+        for t2_results in product(outcomes, repeat=rp2):  
             t2_total = start_points_team2 + sum(t2_results)
             total_cases += 1
             if t2_total > t1_total:
@@ -24,13 +24,10 @@ def simulate_full_5_matches(start_points_team1, start_points_team2):
     return champion_cases / total_cases * 100  # yüzde
 
 
-# 75 puanla başlıyor ve 5 maç oynayacak
-full_5match_prob = simulate_full_5_matches(t1p, t2p)
 
-# Grafik hazırlığı
-labels = ["Şampiyon", "Olamaz"]
-sizes = [full_5match_prob, 100 - full_5match_prob]
-colors = ['gold', 'lightgray']
+full_5match_prob = simulate_full_matches(t1p, t2p)
+
+
 
 fig, ax = plt.subplots()
 ax.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90, colors=colors)
